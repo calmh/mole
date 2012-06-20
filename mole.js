e7d44b1ccff1dc939abab2c5680edcb5580762b1
Jakob Borg <jakob@nym.se>
2012-06-20 12:09:41 +0200

    s/npm install/npm update/

--- mole.js
+++ mole.js
@@ -266,7 +266,7 @@ function pull(opts) {
         if (!err && ver) {
             if (ver !== pkg.version) {
                 con.info('You are using mole v' + pkg.version + '; the latest version is v' + ver);
-                con.info('Use "sudo npm -g install mole" to upgrade');
+                con.info('Use "sudo npm -g update mole" to upgrade');
             } else {
                 con.ok('You are using the latest version of mole');
             }
