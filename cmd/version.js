01f58c23ff15eb96fc01136c74c951d6aa4fe9af
Jakob Borg <jakob@nym.se>
2012-07-17 10:49:23 +0200

    (fix #15) More extensive version report.

--- cmd/version.js
+++ cmd/version.js
@@ -12,14 +12,20 @@ version.prio = 8;
 // 'time to upgrade'-message if there's a mismatch.
 
 function version(opts, state) {
+    con.info('Client: mole v' + state.pkg.version);
+
     libversion.fetch('mole', function (err, ver) {
+        con.info('Latest: mole v' + ver);
         if (!err && ver) {
             if (ver !== state.pkg.version) {
-                con.info('You are using mole v' + state.pkg.version + '; the latest version is v' + ver);
                 con.info('Use "sudo npm -g update mole" to upgrade');
             } else {
                 con.ok('You are using the latest version of mole');
             }
         }
+    });
+
+    state.client.getPkg(function (pkg) {
+        con.info('Server: mole v' + pkg.version);
     });
 }
