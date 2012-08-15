4d000c848553b511e96b8b6a13654fdacace4c15
Jakob Borg <jakob@nym.se>
2012-08-15 20:06:14 +0200

    (fix #20) Disallow push on local repository mismatch.

--- cmd/push.js
+++ cmd/push.js
@@ -25,16 +25,41 @@ function push(opts, state) {
         con.fatal(err);
     }
 
-    // We read the file to a buffer to send to the server. There should be no
-    // errors here since the tunne load and check above succeeded.
+    // Get the list of tunnel definitions from the server. We want the server
+    // version of the tunnel definition to match the local version, if it
+    // exists. A mismatch indicates either that the user edited the file
+    // directly in the local repo (don't!) or that they didn't pull before
+    // editing/pushing. So a very primitive from of conflict prevention.
 
-    con.debug('Reading ' + opts.file);
-    var data = fs.readFileSync(opts.file, 'utf-8');
+    con.debug('Requesting tunnel list from server');
+    state.client.list(function (result) {
+        var base = path.basename(opts.file);
+        var serverside = result.filter(function (i) { return i.name === base; });
+        if (serverside.length === 1) {
+            var local = path.join(state.path.tunnels, base);
+            if (fs.existsSync(local)) {
+                var s = fs.statSync(local);
+                if (s.mtime.getTime() !== serverside[0].mtime) {
+                    con.warning('The local repository version of ' + base + ' is different from the server. Never edit');
+                    con.warning('the files directly in ~/.mole/tunnels and always pull to make sure you have the latest');
+                    con.warning('version before you edit and push. To correct the situation, pull and compare your edits');
+                    con.warning('with the version in ~/.mole/tunnels before pushing again.');
+                    con.fatal('Cowardly refusing to continue.');
+                }
+            }
+        }
 
-    // Send the data to the server. We'll only get the callback if the upload
-    // succeeds.
+        // We read the file to a buffer to send to the server. There should be no
+        // errors here since the tunne load and check above succeeded.
 
-    state.client.send(path.basename(opts.file), data, function (result) {
-        con.ok('Sent ' + data.length + ' bytes');
+        con.debug('Reading ' + opts.file);
+        var data = fs.readFileSync(opts.file, 'utf-8');
+
+        // Send the data to the server. We'll only get the callback if the upload
+        // succeeds.
+
+        state.client.send(base, data, function (result) {
+            con.ok('Sent ' + data.length + ' bytes');
+        });
     });
 }
