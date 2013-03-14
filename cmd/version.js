bc9ad9794e27f958d695117ea4185cc8d0600ad3
Jakob Borg <jakob@nym.se>
2013-03-14 08:56:22 +0100

    (fix #51) Fetch latest mole version directly from npm

--- cmd/version.js
+++ cmd/version.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var debuggable = require('debuggable');
-var libversion = require('version');
+var https = require('https')
 
 var con = require('../lib/console');
 
@@ -13,17 +13,42 @@ debuggable(version);
 // Fetch the latest version number for mole from the npm repository and print a
 // 'time to upgrade'-message if there's a mismatch.
 
+function fetchVersion(pkg, cb) {
+    var options = {
+        host: 'registry.npmjs.org',
+        port: 443,
+        path: '/' + pkg + '/latest',
+        rejectUnauthorized: false
+    };
+
+    var buffer = '';
+    var req = https.request(options, function (res) {
+        if (res.statusCode !== 200)
+            return cb(new Error('Status code ' + res.statusCode));
+        res.setEncoding('utf-8');
+        res.on('data', function (chunk) {
+            buffer += chunk;
+        });
+        res.on('end', function () {
+            var o = JSON.parse(buffer);
+            cb(null, o.version);
+        });
+    });
+    req.end();
+}
+
 function version(opts, state) {
     con.info('Client: mole v' + state.pkg.version);
 
-    libversion.fetch('mole', function (err, ver) {
+    fetchVersion('mole', function (err, ver) {
+        if (err)
+            return con.error('Latest version unknown; ' + err);
+
         con.info('Latest: mole v' + ver);
-        if (!err && ver) {
-            if (ver !== state.pkg.version) {
-                con.info('Use "sudo npm -g update mole" to upgrade');
-            } else {
-                con.ok('You are using the latest version of mole');
-            }
+        if (ver !== state.pkg.version) {
+            con.info('Use "sudo npm -g update mole" to upgrade');
+        } else {
+            con.ok('You are using the latest version of mole');
         }
     });
 
