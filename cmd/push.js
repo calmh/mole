4cb7c150ce865c5c21b2fb55225346e077aa9faf
Jakob Borg <jakob@nym.se>
2013-04-03 18:22:36 +0200

    Push should no longer check local version

--- cmd/push.js
+++ cmd/push.js
@@ -28,50 +28,16 @@ function push(opts, state) {
         con.fatal(err);
     }
 
-    // Get the list of tunnel definitions from the server. We want the server
-    // version of the tunnel definition to match the local version, if it
-    // exists. A mismatch indicates either that the user edited the file
-    // directly in the local repo (don't!) or that they didn't pull before
-    // editing/pushing. So a very primitive from of conflict prevention.
+    // We read the file to a buffer to send to the server. There should be no
+    // errors here since the tunne load and check above succeeded.
 
-    push.dlog('Requesting tunnel list from server');
-    state.client.list(function (result) {
-        var base = path.basename(opts.file);
-        var serverside = result.filter(function (i) { return i.name === base; });
-        if (serverside.length === 1) {
-            var remote = serverside[0];
-            var local = path.join(state.path.tunnels, base);
-            if (fs.existsSync(local)) {
-                var diff = false;
-                if (remote.sha1) {
-                    var sha1 = hashSync(local);
-                    diff = (sha1 !== remote.sha1) ? 'hash' : false;
-                } else {
-                    var s = fs.statSync(local);
-                    diff = (s.mtime.getTime() !== serverside[0].mtime) ? 'mtime' : false;
-                }
-                if (diff) {
-                    con.warning('The local repository version of ' + base + ' differs from the server');
-                    con.warning('version (' + diff + '). Never edit the files directly in ~/.mole/tunnels');
-                    con.warning('and always pull to make sure you have the latest version before');
-                    con.warning('you edit and push. To correct the situation, pull and compare your');
-                    con.warning('edits with the version in ~/.mole/tunnels before pushing again.');
-                    con.fatal('Cowardly refusing to continue.');
-                }
-            }
-        }
+    push.dlog('Reading ' + opts.file);
+    var data = fs.readFileSync(opts.file, 'utf-8');
 
-        // We read the file to a buffer to send to the server. There should be no
-        // errors here since the tunne load and check above succeeded.
+    // Send the data to the server. We'll only get the callback if the upload
+    // succeeds.
 
-        push.dlog('Reading ' + opts.file);
-        var data = fs.readFileSync(opts.file, 'utf-8');
-
-        // Send the data to the server. We'll only get the callback if the upload
-        // succeeds.
-
-        state.client.send(base, data, function (result) {
-            con.ok('Sent ' + data.length + ' bytes');
-        });
+    state.client.send(base, data, function (result) {
+        con.ok('Sent ' + data.length + ' bytes');
     });
 }
