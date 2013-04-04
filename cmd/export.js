99c514f4bc634c58f0c715e9a85e9e4a296d94ea
Jakob Borg <jakob@nym.se>
2013-04-04 16:17:41 +0200

    Export to stdout

--- cmd/export.js
+++ cmd/export.js
@@ -8,13 +8,20 @@ module.exports = exportf;
 exportf.help = 'Export tunnel definition to a file';
 exportf.options = {
     tunnel: { position: 1, help: 'Tunnel name', required: true },
-    file: { position: 2, help: 'File name to write tunnel definition to', required: true },
+    file: { position: 2, help: 'File name to write tunnel definition to' },
 };
 exportf.prio = 5;
 debuggable(exportf);
 
 function exportf(opts, state) {
-    state.client.saveBin('/store/' + opts.tunnel + '.ini', opts.file, function () {
-        con.ok(opts.file);
-    });
+    if (opts.file) {
+        state.client.saveBin('/store/' + opts.tunnel + '.ini', opts.file, function () {
+            con.ok(opts.file);
+        });
+    } else {
+        var req = state.client.bufferedRequest({path: '/store/' + opts.tunnel + '.ini'}, function (result) {
+            console.log(result.buffer);
+        });
+        req.end();
+    }
 }
