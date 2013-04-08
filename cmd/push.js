ae61a670f54c4e00f292a534efebc681fc2e8882
Jakob Borg <jakob@nym.se>
2013-04-08 22:43:51 +0200

    BUG: Add missed filename

--- cmd/push.js
+++ cmd/push.js
@@ -1,10 +1,9 @@
 "use strict";
 
-var debuggable = require('debuggable');
+var con = require('yacon');
 var fs = require('fs');
 var path = require('path');
 
-var con = require('../lib/console');
 var tun = require('../lib/tunnel');
 var hashSync = require('../lib/hash').sync;
 
@@ -14,16 +13,15 @@ push.options = {
     'file': { position: 1, help: 'File name', required: true }
 };
 push.prio = 1;
-debuggable(push);
 
 function push(opts, state) {
     // We load the tunnel, which will cause some validation of it to happen. We
     // don't want to push files that are completely broken.
 
-    push.dlog('Testing ' + opts.file);
+    con.debug('Testing ' + opts.file);
     try {
         tun.loadFile(opts.file);
-        push.dlog('It passed validation');
+        con.debug('It passed validation');
     } catch (err) {
         con.fatal(err);
     }
@@ -31,12 +29,13 @@ function push(opts, state) {
     // We read the file to a buffer to send to the server. There should be no
     // errors here since the tunne load and check above succeeded.
 
-    push.dlog('Reading ' + opts.file);
+    con.debug('Reading ' + opts.file);
     var data = fs.readFileSync(opts.file, 'utf-8');
 
     // Send the data to the server. We'll only get the callback if the upload
     // succeeds.
 
+    var base = path.basename(opts.file);
     state.client.send(base, data, function (result) {
         con.ok('Sent ' + data.length + ' bytes');
     });
