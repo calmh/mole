5b30b880c84230be9b6a1f506cb811e3efad8025
Jakob Borg <jakob@nym.se>
2013-05-21 16:17:23 +0200

    List users

---
+++ cmd/lsusers.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var colors = require('colors');
+var con = require('yacon');
+var table = require('yatf');
+
+module.exports = lsusers;
+lsusers.help = 'List users (requires admin privileges)';
+lsusers.options = {};
+lsusers.prio = 8;
+
+function lsusers(opts, state) {
+    con.debug('Listing users');
+    var rows = [];
+
+    state.client.lsUsers(function (result) {
+        for (var user in result) {
+            rows.push([user.blue.bold, (new Date(result[user].created)).toISOString(), '' + !!result[user].admin, result[user].token]);
+        }
+
+        table(['USER', 'CREATED', 'ADMIN', 'AVAILABLE TOKEN'], rows, { underlineHeaders: true });
+    });
+}
