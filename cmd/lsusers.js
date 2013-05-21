0db71a19422a507924401d5c20150dd5589a506f
Jakob Borg <jakob@nym.se>
2013-05-21 16:20:53 +0200

    Sort user list and handle absent token

--- cmd/lsusers.js
+++ cmd/lsusers.js
@@ -15,8 +15,10 @@ function lsusers(opts, state) {
 
     state.client.lsUsers(function (result) {
         for (var user in result) {
-            rows.push([user.blue.bold, (new Date(result[user].created)).toISOString(), '' + !!result[user].admin, result[user].token]);
+            rows.push([user.blue.bold, (new Date(result[user].created)).toISOString(), '' + !!result[user].admin, result[user].token || '']);
         }
+
+        rows.sort();
 
         table(['USER', 'CREATED', 'ADMIN', 'AVAILABLE TOKEN'], rows, { underlineHeaders: true });
     });
