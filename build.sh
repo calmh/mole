d052d6fce7a1722a025ef68c8bdcac43d1e58874
Jakob Borg <jakob@nym.se>
2013-09-19 10:41:40 +0200

    Get deps on build all

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+pkg=github.com/calmh/mole
 buildstamp=$(date +%s)
 buildver=$(git describe --always)
 builduser="$(whoami)@$(hostname)"
@@ -10,26 +11,28 @@ export GOBIN=$(pwd)/bin
 rm -rf auto
 mkdir auto
 if [[ $1 == "all" ]] ; then
+	go get -d -u -v ./...
+
 	source /usr/local/golang-crosscompile/crosscompile.bash
 	for arch in linux-386 linux-amd64 darwin-amd64 ; do
-		echo $arch
+		echo "$arch"
 		rm -rf bin
-		go-$arch install -ldflags "$ldflags" github.com/calmh/mole/cmd/...
-		tar zcf mole-$arch.tar.gz bin
+		"go-$arch" install -ldflags "$ldflags" "$pkg/cmd/..."
+		tar zcf "mole-$arch.tar.gz" bin
 
-		mv bin/mole auto/mole-$arch
-		mv bin/*/mole auto/mole-$arch
+		mv bin/mole "auto/mole-$arch"
+		mv bin/*/mole "auto/mole-$arch"
 		hash=$(sha1sum auto/mole-$arch | awk '{print $1}')
-		echo "{\"buildstamp\":$buildstamp, \"version\":\"$buildver\", \"hash\":\"$hash\"}" >> auto/mole-$arch.json
-		gzip -9 auto/mole-$arch
+		echo "{\"buildstamp\":$buildstamp, \"version\":\"$buildver\", \"hash\":\"$hash\"}" >> "auto/mole-$arch.json"
+		gzip -9 "auto/mole-$arch"
 	done
 	for arch in windows-386 windows-amd64 ; do
-		echo $arch
+		echo "$arch"
 		rm -rf bin
-		go-$arch install -ldflags "$ldflags" github.com/calmh/mole/cmd/...
-		zip -r mole-$arch.tar.zip bin
+		"go-$arch" install -ldflags "$ldflags" "$pkg/cmd/..."
+		zip -r "mole-$arch.tar.zip" bin
 	done
 else
-	go install -ldflags "$ldflags" github.com/calmh/mole/cmd/...
+	go install -ldflags "$ldflags" "$pkg/cmd/..."
 fi
 
