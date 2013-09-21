1a3531bb3f1f1825449afe27761ca0e6391662ea
Jakob Borg <jakob@nym.se>
2013-09-21 14:12:31 +0200

    LDAP backed ticket authentication for v4 clients

--- cmd/server.js
+++ cmd/server.js
@@ -7,7 +7,9 @@ module.exports = startServer;
 startServer.help = 'Start a mole server instance';
 startServer.options = {
     port: { abbr: 'p', help: 'Set listen port [9443]', default: '9443' },
-    store: { abbr: 's', help: 'Set store directory [~/mole-store]', default: path.join(process.env.HOME, 'mole-store') }
+    store: { abbr: 's', help: 'Set store directory [~/mole-store]', default: path.join(process.env.HOME, 'mole-store') },
+    ldapsrv: { help: 'Set LDAP server [ldap://127.0.0.1]', default: 'ldap://127.0.0.1' },
+    ldapdn: { help: 'Set LDAP bind DN pattern [uid=%s,cn=users,cn=accounts,dc=example,dc=com]', default: 'uid=%s,cn=users,cn=accounts,dc=example,dc=com' },
 };
 startServer.prio = 9;
 
