d243a14b6036242a988c29ec0c223dfeace542c8
Jakob Borg <jakob@nym.se>
2013-10-09 10:54:15 +0200

    Implement read only mode

--- cmd/server.js
+++ cmd/server.js
@@ -10,6 +10,7 @@ startServer.options = {
     store: { abbr: 's', help: 'Set store directory [~/mole-store]', default: path.join(process.env.HOME, 'mole-store') },
     ldapsrv: { help: 'Set LDAP server [ldap://127.0.0.1]', default: 'ldap://127.0.0.1' },
     ldapdn: { help: 'Set LDAP bind DN pattern [uid=%s,cn=users,cn=accounts,dc=example,dc=com]', default: 'uid=%s,cn=users,cn=accounts,dc=example,dc=com' },
+    ro: { help: 'Set server to read only mode, with the specified reason returned to clients who attempt writes.'},
 };
 startServer.prio = 9;
 
